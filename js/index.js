41b7c454d5fc6d791c9f57f375ad18b7f96fd1db
Adam Ralph <adamralph@posteo.net>
2022-10-02 10:29:19 +1100

    init

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// const question = document.getElementById("question-1");
+// const log = document.getElementById("log");
+
+// var score = {};
+
+// question.addEventListener(
+//   "submit",
+//   (e) => {
+//     const data = new FormData(question);
+//     let output = "";
+//     console.log("Clicked!");
+//     for (const entry of data) {
+//       // localStorage.setItem(score);
+//       if (entry[1] === "correct") {
+//         score.q1 = "Correct";
+//         output = `That is the correct answer! ${score.q1}`;
+//       } else {
+//         score.q1 = "Incorrect";
+//         output = "sorry, that is not the right answer";
+//       }
+//     }
+//     log.innerText = output;
+//     e.preventDefault();
+//   },
+//   false
+// );
