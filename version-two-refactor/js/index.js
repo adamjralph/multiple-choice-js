59f8d94c353bbd233d4a0adf60297f27ad8e04fb
Adam Ralph <adamralph@posteo.net>
2022-10-02 16:10:26 +1100

    working

--- version-two-refactor/js/index.js
+++ version-two-refactor/js/index.js
@@ -1,35 +1,44 @@
 const question = document.getElementById("question");
 const log = document.getElementById("log");
-const nextButtonParent = document.getElementById("next-button-parent");
-let questionNum = Number(question.dataset.number);
+const questionNum = Number(question.dataset.number);
+console.log(questionNum);
 
-const nextButton = `<a href="q${questionNum + 1}.html">
-<button class="btn next-btn">Next Question</button>
-</a>`;
+function addNextButton() {
+  const questionNum = Number(question.dataset.number);
+  const nextButtonParent = document.getElementById("next-button-parent");
+  const nextButton = `<a href="q${questionNum + 1}.html">
+  <button class="btn next-btn">Next Question</button>
+  </a>`;
+  nextButtonParent.innerHTML = nextButton;
+  console.log({ nextButtonParent, questionNum, nextButtonParent });
+}
 
 question.addEventListener(
   "submit",
   (e, questionNum) => {
     const data = new FormData(question);
     let output = "";
-    console.log(localStorage.getItem(questionNum));
-    localStorage.clear();
+    // console.log(localStorage.getItem(questionNum));
+    if (localStorage.getItem(questionNum) === null) {
+      console.log("Nothing selected");
+      output = "Please select an answer";
+    }
     for (const entry of data) {
+      console.log(entry[1]);
       if (entry[1] === "correct") {
         localStorage.setItem(questionNum, 1);
         output = `That is the correct answer!`;
-      } else {
+        addNextButton();
+      }
+      if (entry[1] === "no") {
         localStorage.setItem(questionNum, 0);
         output = "Sorry, that is not the right answer";
+        addNextButton();
       }
       console.log(localStorage.getItem(questionNum));
     }
-    if (localStorage.getItem(questionNum) === null) {
-      log.innerText = "Please select an answer";
-    } else {
-      nextButtonParent.innerHTML = nextButton;
-    }
     log.innerText = output;
+    localStorage.clear();
     e.preventDefault();
   },
   false
